e69a927ef51899ebb56b5cddf5ab091775d69ac5
dsareen <dk930654@dal.ca>
2025-02-01 19:00:37 -0500

    feat: updated package json

--- src/components/Body.js
+++ src/components/Body.js
@@ -44,14 +44,13 @@ const Body = () => {
           </div>
         </div>
         <div className="hidden md:block">
-          <figure className="relative w-[300px] h-[450px] ml-auto rounded-lg overflow-hidden shadow-2xl m-auto">
-            <div className="absolute inset-0 bg-gradient-to-l from-zinc-900 via-transparent to-zinc-900 rounded-lg"></div>
-            <div className="absolute inset-0 bg-gradient-to-t from-zinc-900 via-transparent to-zinc-900 rounded-lg"></div>
+          <figure className="relative w-[300px] h-[450px] ml-auto overflow-hidden shadow-2xl m-auto">
+            <div className="absolute inset-0 bg-gradient-to-l from-zinc-900 via-transparent to-zinc-900 "></div>
 
             <img
               src="/images/deeksha.jpeg"
               alt="Deeksha Sareen"
-              className="w-full h-full object-cover rounded-lg transition-transform duration-300 hover:scale-110 "
+              className="w-full h-full object-cover transition-transform duration-300 hover:scale-110 "
               width={450}
               height={650}
             />
